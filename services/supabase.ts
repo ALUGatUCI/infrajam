b2820942cdb0ad1a8df9647da594fbef6fa92400
robotman40 <robotman40@protonmail.com>
2026-06-28 23:47:41 +0900

    Implement initial version of the Supabase service (proper)

--- services/supabase.ts
+++ services/supabase.ts
@@ -1,8 +1,43 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js'
 
-const supabase: SupabaseClient = createClient(
+class DatabaseService {
+  private supabase: SupabaseClient
+
+  public constructor() {
+    this.supabase = createClient(
       process.env.SUPABASE_URL!,
       process.env.SUPABASE_SERVICE_ROLE_KEY!,
     )
+  }
 
-export default supabase
+  public async confirmApplication(appId: string, confirmationCode: string): Promise<void> {
+    const { data, error } = await this.supabase
+      .from('applications')
+      .select('confirmed, confirmation_code')
+      .eq('id', appId)
+      .maybeSingle()
+
+    if (error) {
+      throw new Error("Application was not found")
+    }
+
+    if (data?.confirmed) {
+      throw new Error("Application has already been confirmed")
+    }
+
+    if (confirmationCode == data?.confirmation_code) {
+      const { error: updateError } = await this.supabase
+        .from('applications')
+        .update({ confirmed: true })
+        .eq('id', appId)
+
+      if (updateError) {
+        throw new Error("An error occurred confirming your application")
+      }
+    } else {
+      throw new Error("The confirmation code does not match")
+    }
+  }
+}
+
+export default DatabaseService
